ddfd115454dfa8187ec061bb08f25fabb071e703
wwwhoami <subman450@gmail.com>
2024-03-17 02:55:43 +0300

    4th stage retry(3)

--- src/main.rs
+++ src/main.rs
@@ -19,22 +19,25 @@ async fn handle(mut socket: TcpStream) {
     const PING: &[u8] = b"*1\r\n$4\r\nping\r\n";
     let mut buf = [0; 512];
 
-    match socket.read(&mut buf).await {
-        Ok(n) => {
-            println!("Got: {:?}", &buf[..n]);
+    loop {
+        match socket.read(&mut buf).await {
+            Ok(0) => return,
+            Ok(n) => {
+                println!("Got: {:?}", &buf[..n]);
 
-            let out_buf = match &buf[..n] {
-                PING => "+PONG\r\n",
-                _ => "-Error unkown command\r\n",
-            };
+                let out_buf = match &buf[..n] {
+                    PING => "+PONG\r\n",
+                    _ => "-Error unkown command\r\n",
+                };
 
-            if socket.write_all(out_buf.as_bytes()).await.is_err() {
-                eprintln!("Unexpected socket error while writing to buffer")
+                if socket.write_all(out_buf.as_bytes()).await.is_err() {
+                    eprintln!("Unexpected socket error while writing to buffer")
+                }
+                println!("Responded: {:?}", out_buf);
             }
-            println!("Responded: {:?}", out_buf);
-        }
-        Err(e) => {
-            eprintln!("Unexpected socket error: {}", e);
+            Err(e) => {
+                eprintln!("Unexpected socket error: {}", e);
+            }
         }
     }
 }
